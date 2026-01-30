4bf0649271b79a137b46764e8b82fef1bcf8b83e
mybookG <gil0jin@gmail.com>
2026-01-31 01:00:01 +0900

    [REFECTOR] #10 TagRepository 파일 추가

---
+++ src/main/java/com/umc/greaming/domain/tag/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package com.umc.greaming.domain.tag.repository;
+
+import com.umc.greaming.domain.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByName(String name);
+}
